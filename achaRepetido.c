397279e5b603d52438131cf4826822d53a3437f1
biankavm <bianka.vm03@gmail.com>
2022-07-10 06:39:34 -0300

    Função para encontrar o elemento que mais se repete em um vetor

--- achaRepetido.c
+++ achaRepetido.c
@@ -1,29 +1,33 @@
-/*Encontrar o elemento que mais se repete em um vetor.*/
+/*Faça uma função que receba como parâmetro um vetor de números inteiros e o seu tamanho, considerando
+que o vetor não foi fornecido ordenado. A função deve descobrir qual o número que mais se repete e retornar
+esse número. Em hipótese alguma a função deve gerar um novo vetor para descobrir qual número mais se repete;*/
 
 #include <stdio.h>
 
 int achaRepetido(int vet[], unsigned tam){
-    int i, j, repetido, c1, c2;
-    c2 = 0;
+    int i, j, repetido, pivot;
+    unsigned cont1, cont2;
+    cont1 = 1;
     for(i = 0; i < tam; i++){
-        c1 = 0; // inicializa o primeiro contador
-        for(j = i+1; j < tam; j++){
-            if (vet[i] == vet[j]){
-                c2++; // add no contador 2
+        pivot = vet[i];
+        cont2 = 1; // 
+        for (j = i + 1; j < tam; j++){
+            if (vet[j] == pivot){
+                cont2++;
             }
-            if (c2> c1){ // se o 2 for maior que o 1
-                c1 = c2; // o primeiro recebe o valor do segundo e o repetido é o valor dessa posição.
-                repetido = vet[i];
-            }
-            }
+            if(cont2 > cont1){
+                cont1 = cont2;
+                repetido = pivot;
+            } 
         }
+    }
     return repetido;
 }
 
 void lerVetor(int vet[], unsigned tam){
     int i;
-    for (i=0; i< tam; i++){
-        printf("Digite um elemento para a posição [%d]: ", i);
+    for (i = 0; i < tam; i++){
+        printf("Digite o número para a posição [%d]: ", i);
         scanf("%d", &vet[i]);
     }
     printf("\n");
@@ -31,24 +35,19 @@ void lerVetor(int vet[], unsigned tam){
 
 void mostraVetor(int vet[], unsigned tam){
     int i;
-    for (i = 0; i<tam;i++){
+    for (i = 0; i < tam; i++){
         printf("%d ", vet[i]);
     }
     printf("\n");
 }
 
-
-
-
-int main()
-{
-    unsigned tam;
+int main(){
+    int tamanho;
     printf("Entre com o tamanho do vetor: ");
-    scanf("%d", &tam);
-    int vetor[tam];
-    lerVetor(vetor, tam);
-    mostraVetor(vetor,tam);
-    printf("O elemento que mais se repete é %d.", achaRepetido(vetor, tam));
-
-    return 0;
+    scanf("%d", &tamanho);
+    int vet[tamanho];
+    lerVetor(vet, tamanho);
+    printf("O vetor digitado foi: ");
+    mostraVetor(vet, tamanho);
+    printf("\nO número que mais se repete é o %d.", achaRepetido(vet, tamanho));
 }
